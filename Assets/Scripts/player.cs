f67999ceb03c4d39605c3306749a99b5fad59f19
lara-ocon <202115710@alu.comillas.edu>
2023-11-07 23:10:45 +0100

    Añado game object de los bordes e impido al jugador salirse de estos

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -16,28 +16,39 @@ public class Player : MonoBehaviour
     public int diceNumber = 1;
 
     private Vector2 moveDirection = Vector2.zero;
+    private Vector2 jumpDirection = Vector2.zero;
     private float currentVelocity;
     private float mouseAngle;
+
+    // Limits of the game
+    private BoxCollider2D gameBorderCollider;
+
+    // Initialization
+    private void Start()
+    {
+        gameBorderCollider = GameObject.Find("GameBorder").GetComponent<BoxCollider2D>();
+    }
+
 
     private void Update()
     {
         moveDirection = Vector2.zero;
-        // Movimiento a la derecha si se presiona la tecla D
+        // Movement to the right if D is pressed
         if (Input.GetKey(KeyCode.D))
         {
             moveDirection += Vector2.right;
         }
-        // izquierda
+        // Left if A is pressed
         if (Input.GetKey(KeyCode.A))
         {
             moveDirection += Vector2.left;
         }
-        // arriba (en eje Y)
+        // Up if W is pressed
         if (Input.GetKey(KeyCode.W))
         {
             moveDirection += Vector2.up;
         }
-        // abajo
+        // Down if S is pressed
         if (Input.GetKey(KeyCode.S))
         {
             moveDirection += Vector2.down;
@@ -60,7 +71,14 @@ public class Player : MonoBehaviour
     }
 
     private void FixedUpdate()
-    {
+    {   
+        // Check if the player stays inside the game borders
+        // If not, the movement will be the maximum possible inside the borders
+        if (!isInsideBorders(moveDirection))
+        {
+            moveDirection = getMovementInsideBorders(moveDirection);
+        }
+
         // Movimiento del jugador independiente de la rotacion
         transform.Translate(moveDirection, Space.World);
 
@@ -100,14 +118,55 @@ public class Player : MonoBehaviour
             Mathf.Sin(mouseAngle * Mathf.Deg2Rad)
         );
 
-        transform.position += new Vector3(
-            jumpDirection.x * diceNumber,
-            jumpDirection.y * diceNumber,
-            0
-        );
+
+        jumpDirection.x = jumpDirection.x * diceNumber;
+        jumpDirection.y = jumpDirection.y * diceNumber;
+
+        // Check if the player stays inside the game borders
+        if (!isInsideBorders(jumpDirection))
+        {
+            jumpDirection = getMovementInsideBorders(jumpDirection);
+        }
+
+        // Teletransportamos al jugador a la posición del salto
+        transform.Translate(jumpDirection, Space.World);
 
         // Cambiamos el numero de la cara del dado
         diceNumber = Random.Range(1, 7);
+    }
+
+    // Check if the movement takes the player outside the game borders
+    private bool isInsideBorders(Vector2 movement)
+    {
+        return gameBorderCollider.bounds.Contains(transform.position + (Vector3)movement);
+    }
+
+    // Get the movement that the player should do to stay inside the borders
+    private Vector2 getMovementInsideBorders(Vector2 movement)
+    {
+        Vector2 movementInsideBorders = movement;
+
+        // We check what border is the one that the player is trying to cross, and we take the 
+        // player to that point in that specific border
+        if (transform.position.x + movement.x > gameBorderCollider.bounds.max.x)
+        {
+            movementInsideBorders.x = gameBorderCollider.bounds.max.x - transform.position.x;
+        }
+        else if (transform.position.x + movement.x < gameBorderCollider.bounds.min.x)
+        {
+            movementInsideBorders.x = gameBorderCollider.bounds.min.x - transform.position.x;
+        }
+
+        if (transform.position.y + movement.y > gameBorderCollider.bounds.max.y)
+        {
+            movementInsideBorders.y = gameBorderCollider.bounds.max.y - transform.position.y;
+        }
+        else if (transform.position.y + movement.y < gameBorderCollider.bounds.min.y)
+        {
+            movementInsideBorders.y = gameBorderCollider.bounds.min.y - transform.position.y;
+        }
+
+        return movementInsideBorders;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
